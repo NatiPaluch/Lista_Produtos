915c6d562a2ce903bfaa36900baab0e02cba7d5c
Natipaluch <natipaluch2@gmail.com>
2025-07-13 17:51:01 -0300

    atualizacoes 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,20 +10,16 @@
 <h3>Bem-vindo à Lunaticos, o lugar perfeito para os verdadeiros apaixonados por games, filmes, séries e cultura nerd! Aqui você encontra:</h3><br><br>
 =>Posters e Quadros – Transforme seu cantinho com artes incríveis dos seus universos favoritos.<br>
 
-<div class="img-cont">
-<img src=https://cdn.awsli.com.br/600x700/186/186813/produto/53781434/f6cc7e4aa3.jpg class="img-cont" style="max-width: 200px;">
-</div>
+
 
 =>Stickers Metálicos – Adesivos premium e resistentes para personalizar seus gadgets, cadernos e muito mais.<br>
-<div class="img-cont">
-<img src=https://i.etsystatic.com/36044742/r/il/50018c/4258598815/il_1080xN.4258598815_5n3b.jpg class="img-cont" style="max-width: 200px;">
-</div>
 
 =>Stickers Comuns – Cole em qualquer lugar e mostre sua paixão com designs exclusivos.<br>
+
 <div class="img-cont">
 <img src=https://i.pinimg.com/474x/28/af/97/28af9751df1f60f5fec9078673a127c2.jpg class="img-cont" style="max-width: 200px;">
-</div>
-
+<img src=https://i.etsystatic.com/36044742/r/il/50018c/4258598815/il_1080xN.4258598815_5n3b.jpg class="img-cont"  style="max-width: 150px;">
+<img src=https://cdn.awsli.com.br/600x700/186/186813/produto/53781434/f6cc7e4aa3.jpg class="img-cont"  style="max-width: 200px;"></div>
 Tudo 100% online, com entregas rápidas e produtos de alta qualidade para você expressar sua loucura geek!<br>
 
 Lunaticos – Sua loja de adesivos, posters e tudo que um verdadeiro fã precisa!</p>
